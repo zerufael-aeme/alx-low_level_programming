311f6b143e6d83c6594455afcfaa516e9c338eb9
Zerufael Aemero <zerufaelaemero@gmail.com>
2023-10-11 08:41:37 -0700

    Nested Loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints the alphabet in lowercase
+ * and uses the control loop for, and the function putchar
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n")
+	return (0);
+}
