3065ab546d607165dffaeacf87c558e733619235
Zerufael Aemero <zerufaelaemero@gmail.com>
2023-10-17 12:06:34 -0700

    Arrays & Pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strlen - This function returns the length of a string
+ * @s: Variable that contains a character array to be counted
+ * Return: Length of the string
+ */
+int _strlen(char *s)
+{
+return strlen(*s);
+}
