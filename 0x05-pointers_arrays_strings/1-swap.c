022cc8644c832ad664df2d2217c0b1c06a99fe7c
Zerufael Aemero <zerufaelaemero@gmail.com>
2023-10-17 11:58:54 -0700

    Arrays & Pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * swap_int - This function swaps the values of two integers
+ * @a: First integer whose value is to be swapped with b
+ * @b: Second integer whose value is to be swapped with a
+ */
+void swap_int(int *a, int *b)
+{
+int temp;
+
+temp = *a;
+*a = *b;
+*b = temp;
+}
