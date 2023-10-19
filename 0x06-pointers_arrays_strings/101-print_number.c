0daddee99771a57c3841a82ff8b54d802e91970e
Zerufael Aemero <zerufaelaemero@gmail.com>
2023-10-19 14:03:21 -0700

    More Arrays & Pointers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,16 +1,34 @@
 #include "main.h"
 
 /**
- * print_number - prints a number in reverse order
- * @n: number to print
- *
- * Return: void
+ * print_number - This function prints an integer
+ * @n: Integer to evaluate
  */
+
 void print_number(int n)
 {
-    if (n != 0)
-    {
-        print_number(n / 10);
-        putchar((n % 10) + '0');
-    }
+	
+	if (n / 10)
+	{
+		
+		print_number(n / 10);
+		
+		n %= 10;
+		
+		if (n < 0)
+			
+			n *= -1;
+	}
+	
+	else
+	
+		if (n < 0)
+		{
+			
+			_putchar('-');
+			
+			n *= -1;
+		}
+	
+	_putchar(n + '0');
 }
