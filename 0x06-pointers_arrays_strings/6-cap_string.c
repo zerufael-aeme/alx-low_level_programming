43096f7c2045c11ba32406cd14dcbe93b121fbb9
Zerufael Aemero <zerufaelaemero@gmail.com>
2023-10-19 09:51:36 -0700

    More Arrays & Pointers

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * cap_string - This function capitalizes all words of a string
+ * @s: String to evaluate
+ * Return: The string converted
+ */
+char *cap_string(char *s)
+{
+	int i, ch;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		if (s[i] == ' ')
+		{
+			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+			{
+				ch = s[i + 1];
+			        ch = ch - 32;
+			        s[i + 1] = ch;
+			}
+		}
+	}
+	retrun (s);
+}
