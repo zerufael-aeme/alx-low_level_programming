6772bc680df6c4ff74b11a3c7ac72839d70fe8b9
Zerufael Aemero <zerufaelaemero@gmail.com>
2023-11-01 11:48:43 -0700

    Argc & Argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *main - Prints its name
+ *@argc: number of argument
+ *@argv: Argumnet vector
+ *Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
