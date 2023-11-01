1928b5b948ce8f6def9d2e73410f5d57cd466772
Zerufael Aemero <zerufaelaemero@gmail.com>
2023-11-01 12:52:23 -0700

    Argc & Argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -17,11 +17,9 @@ int main(int argc, char *argv[])
 	{
 		printf("%s\n", "Error");
 		return (1);
-	}else
-	{
-		result = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", result);
 	}
+	result = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", result);
 	return (0);
 
 }
