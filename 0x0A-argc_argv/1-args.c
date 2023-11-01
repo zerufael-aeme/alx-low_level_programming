dadbf320a487127179446f2b4d84dce249d95cb4
Zerufael Aemero <zerufaelaemero@gmail.com>
2023-11-01 11:56:22 -0700

    Argc & Argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - A program that print the number of arguements you passed into it
+ *
+ * @argc: An argument counter
+ *
+ * @argv: An argurmnt values
+ *
+ * Return: Always o (success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
