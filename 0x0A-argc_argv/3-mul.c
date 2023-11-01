c920f55408fa6a0db58906a9fb89b1eb71a57a77
Zerufael Aemero <zerufaelaemero@gmail.com>
2023-11-01 12:37:59 -0700

    Argc & Argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - A program that multiplies two numbers
+ * @argc: The arguments' counter
+ * @argv: The arguments' values
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int result = 0;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	} else
+	{
+		result = atoi(argv[1]) + atoi(argv[2]);
+		return (result);
+	}
+
+}
