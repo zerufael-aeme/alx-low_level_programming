f016ce63c6b850d3b50f09955f42d620b9f139a3
Zerufael Aemero <zerufaelaemero@gmail.com>
2023-11-09 11:18:13 -0800

    Vardic Functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,19 +10,21 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum;
+	unsigned int i;
 	va_list pa;
 
 	sum = 0;
-	va_start (ap, n);
+	i = 0;
+	va_start (pa, n);
 
 	if (n == 0)
                 return (0);
 
 	for (i = 0; i < n;i++)
 	{
-		sum += va_arg (ap, int);
+		sum += va_arg (pa, int);
 	}
-	va_end (ap);
+	va_end (pa);
 
 	return (sum);
 
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,25 @@
+#ifndef VARIADIC_FUNCRIONS_
+#define VARIADIC_FUNCRIONS_
+
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * struct print - print type with corresponding print function
+ * @t: print type
+ * @f: print function
+ */
+typedef struct print
+{
+	char *t;
+	void (*f)(va_list);
+} print_t;
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+
+#endif /* VARIADIC_FUNCRIONS_ */
