5da50e6c4239bd734cc00e3713e4bbc8bcde3390
Zerufael Aemero <zerufaelaemero@gmail.com>
2023-11-09 12:09:18 -0800

    Vardic Functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	va_list num;
+
+	va_start(num, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d",va_arg(num, int));
+		if (separator != NULL)
+			printf("%s",separator);
+	}
+	printf("\n");
+}
