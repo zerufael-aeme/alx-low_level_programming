a5b324402a1d6ddbf6def9a10ac950141e97ce62
Zerufael Aemero <zerufaelaemero@gmail.com>
2023-11-09 12:22:50 -0800

    Vardic Functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -5,20 +5,24 @@
  * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-	va_list num;
+	va_list list;
 
-	va_start(num, n);
+	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(num, int));
-		if (separator)
-			printf("%s", separator);
+		if (!separator)
+			printf("%d", va_arg(list, int));
+		else if (separator && i == 0)
+			printf("%d", va_arg(list, int));
+		else
+			printf("%s%d", separator, va_arg(list, int));
 	}
-	va_end(num);
+
+	va_end(list);
+
 	printf("\n");
 }
